625ac4252a31303f558a6341d9ffdea428fd5b6a
Vicky Mahato <142152599+Vickym78@users.noreply.github.com>
2024-03-17 15:58:15 +0530

    Add files via upload
    js assignment 1

---
+++ 7.js
@@ -0,0 +1,6 @@
+/* S/ You are buildin2 a simple shoppin2 list app. You have the items name in an array. Write a pro2ram that uses
+a for loop to print all the items in the shoppin2 list array/ */
+let arr=[ "Apple" ,"Banana","Carrot"];
+for(let val  of  arr){
+        console.log(val);
+}
---
+++ 8.js
@@ -0,0 +1,8 @@
+/* You are creatin2 a countdown app. Implement a pro2ram that uses a while loop to count down from 10 to 1
+and prints each number. */
+let val =10;
+
+while(val>0){
+    console.log(val);
+    val--;
+}
