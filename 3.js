dce776602bd4e26ed1f293b7100abffdee655bd8
Vicky Mahato <142152599+Vickym78@users.noreply.github.com>
2024-03-17 16:03:21 +0530

    Delete 3.js

--- 3.js
+++
@@ -1,7 +0,0 @@
-/* You are working on a -urren-y -onversion appli-ation+ Write a program that has a variable whi-h stores the
-amount in Indian Rupees (INR) and prints the equivalent amount in US Dollars (USD)+ Use the -urrent
-ex-hange rate of 1 USD = 82 INR+ */
-
-let  inr = 850 ;
-let usd = inr/82;
-console.log(`${inr} INR is ${usd} USD`);
