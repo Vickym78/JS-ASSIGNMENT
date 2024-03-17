ae212b11b930dd8aceb02de027eff7d8290cedb1
Vicky Mahato <142152599+Vickym78@users.noreply.github.com>
2024-03-17 16:02:02 +0530

    Add files via upload
    for 1st Asignment

---
+++ Assignments/1.js
@@ -0,0 +1,10 @@
+/**
+ Suppose you are building a payment -he-kout page and must display the final pri-e after the dis-ount+
+Create a simple dis-ount -al-ulator that takes two values from the variables - the total -ost and the
+dis-ount per-entage - and prints the dis-ounted value.
+ */
+
+let total = 2000;
+let discount =20;
+let ans= 2000- ((2000*20)/100)
+ console.log(`The final price is ${ans}`);
---
+++ Assignments/4.js
@@ -0,0 +1,12 @@
+/*You are developing a booking appli-ation for a -inema+ The ti-ket pri-e depends on the type of the
+viewer("-hild", "adult", or "senior")+ Write a program whi-h -al-ulates the pri-e a--ordingly and prints the total
+pri-e to be paid+ Let’s assume the ti-ket pri-e for a -hild is Rs+ 100, adult ti-ket pri-e is Rs+ 150 and ti-ket pri-e
+for a senior is Rs+ 120+ */
+
+let na =1;
+let nc= 2;
+let ls =1;
+
+let val =0;
+val = (na*150) + (nc*100) +(ls*120);
+console.log(`The total ticket price is ${val}`);
