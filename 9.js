9976f3f74b8a770f64e921fb5a5e247b97d77fd4
Vicky Mahato <142152599+Vickym78@users.noreply.github.com>
2024-03-17 16:04:29 +0530

    Delete 9.js

--- 9.js
+++
@@ -1,8 +0,0 @@
-/* j/ You are 2iven an array of numbers and strin2s. Your task is to find the first strin2 in the array. On findin2 the
-first strin2 print “Found the First Strin2” and its value. */
-
-const arr =[1,2,3,999,56,"Mithun",1234,"pw"];
-
-for (let i=0;i<arr.length ;i++){
-    if(typeof arr[i] =='string') {console.log("First string found :", arr[i]); break;} 
-}
